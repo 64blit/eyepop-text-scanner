571bb30f1cf55d888a6762edb975c868c7968584
Edmund Dao <61922101+64blit@users.noreply.github.com>
2024-06-18 10:59:53 -0700

    [update] changed text scanner to cereal

--- client/components/ResultsOverlay.jsx
+++ client/components/ResultsOverlay.jsx
@@ -24,7 +24,7 @@ export const ResultsOverlay = ({ title, labelsList }) =>
                         <div className="z-20 flex flex-row gap-2 w-[80%] m-[10%]  flex-wrap   p-5 rounded-xl bg-gray-800">
                             <div className="text-4xl text-white"> Text found: </div>
                             <a
-                                href={`https://www.google.com/search?q=${labelsList.join(", ") + " cocktail recipe"}`}
+                                href={`https://www.google.com/search?q=${labelsList.join(", ") + " tiktok video recipe"}`}
                                 target="_blank"
                                 rel="noopener noreferrer"
                                 className="text-4xl text-blue-300 underline pointer-events-auto"
@@ -34,7 +34,8 @@ export const ResultsOverlay = ({ title, labelsList }) =>
                             </a>
                         </div>
                     }
-                </div>}
+                </div>
+            }
         </>
     );
 
